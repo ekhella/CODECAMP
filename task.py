07704a45c72e35889aee760975ad49c9d3a4db0b
Eric <khellaeric@gmail.com>
2024-09-09 17:27:18 +0200

    Définition des fonctionsé

--- task.py
+++ task.py
@@ -1,2 +1,13 @@
-print ("Hello WOrld !")
-print ("Test")
+import argparse
+
+def add():
+    return None
+
+def modify():
+    return None
+
+def rm():
+    return None
+
+def show():
+    return None
